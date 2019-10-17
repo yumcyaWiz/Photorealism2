5d111dd2dc6eabdd89e0ba442346529d7ec12da1
yumcyawiz <hashdolf1923@gmail.com>
2019-10-17 17:54:53 +0900

    impl some funcs on mat4

--- src/matrix4.h
+++ src/matrix4.h
@@ -30,4 +30,20 @@ struct alignas(64) Matrix4 {
     m[3][0] = m30; m[3][1] = m31; m[3][2] = m32; m[3][3] = m33;
   };
 };
+
+
+inline Matrix4 identity() {
+  return Matrix4(1, 0, 0, 0,
+                 0, 1, 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1);
+}
+
+
+inline Matrix4 transpose(const Matrix4& m) {
+  return Matrix4(m[0][0], m[1][0], m[2][0], m[3][0],
+                 m[0][1], m[1][1], m[2][1], m[3][1],
+                 m[0][2], m[1][2], m[2][2], m[3][2],
+                 m[0][3], m[1][3], m[2][3], m[3][3]);
+}
 #endif
