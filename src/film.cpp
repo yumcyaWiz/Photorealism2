18ba5f0561d464ed8c7f7b2faf81d693e74e20c4
yumcyawiz <hashdolf1923@gmail.com>
2019-11-02 20:18:53 +0900

    gamma correction when writePPM

--- src/film.cpp
+++ src/film.cpp
@@ -14,16 +14,17 @@ void Film::writePPM(const std::string& filename) const {
       const SPD spd = getPixel(i, j);
 
       // RGBを計算
+      // ガンマ補正も同時に行う
       const RGB rgb = spd.toRGB();
-      const unsigned int r = std::clamp(static_cast<unsigned int>(255 * rgb.x),
-                                        static_cast<unsigned int>(0),
-                                        static_cast<unsigned int>(255));
-      const unsigned int g = std::clamp(static_cast<unsigned int>(255 * rgb.y),
-                                        static_cast<unsigned int>(0),
-                                        static_cast<unsigned int>(255));
-      const unsigned int b = std::clamp(static_cast<unsigned int>(255 * rgb.z),
-                                        static_cast<unsigned int>(0),
-                                        static_cast<unsigned int>(255));
+      const unsigned int r = std::clamp(
+          static_cast<unsigned int>(255 * std::pow(rgb.x, 1 / 2.2)),
+          static_cast<unsigned int>(0), static_cast<unsigned int>(255));
+      const unsigned int g = std::clamp(
+          static_cast<unsigned int>(255 * std::pow(rgb.y, 1 / 2.2)),
+          static_cast<unsigned int>(0), static_cast<unsigned int>(255));
+      const unsigned int b = std::clamp(
+          static_cast<unsigned int>(255 * std::pow(rgb.z, 1 / 2.2)),
+          static_cast<unsigned int>(0), static_cast<unsigned int>(255));
 
       //書き込み
       file << r << " " << g << " " << b << std::endl;
