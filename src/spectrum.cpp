e549c78f97c01e84020dc226ccbacb72e507d0b9
yumcyawiz <hashdolf1923@gmail.com>
2019-11-02 20:19:05 +0900

    impl spd to xyz

--- src/spectrum.cpp
+++ src/spectrum.cpp
@@ -26,4 +26,42 @@ void SPD::addSample(const Real& _l, const Real& _phi) {
   }
 }
 
-XYZ SPD::toXYZ() const { return XYZ(); }
+//等色関数は線形補間して使用する
+XYZ SPD::toXYZ() const {
+  XYZ xyz;
+
+  for (std::size_t i = 0; i < lambda.size(); ++i) {
+    const Real lambda_value = lambda[i];
+    const Real phi_value = phi[i];
+
+    //放射束が0の場合は計算をスキップ
+    if (phi_value == 0) continue;
+
+    //対応する等色関数のインデックスを計算
+    const unsigned int index = (lambda_value - 380) / 5;
+
+    //等色関数を線形補間
+    Real cmf_x, cmf_y, cmf_z;
+    if (index != color_matching_func_samples - 1) {
+      const unsigned int cmf_lambda = 5 * index + 380;
+      const Real t = static_cast<Real>(lambda_value - cmf_lambda) / 5;
+      cmf_x = (1.0f - t) * color_matching_func_x[index] +
+              t * color_matching_func_x[index + 1];
+      cmf_y = (1.0f - t) * color_matching_func_y[index] +
+              t * color_matching_func_y[index + 1];
+      cmf_z = (1.0f - t) * color_matching_func_z[index] +
+              t * color_matching_func_z[index + 1];
+    } else {
+      cmf_x = color_matching_func_x[index];
+      cmf_y = color_matching_func_y[index];
+      cmf_z = color_matching_func_z[index];
+    }
+
+    // XYZを計算
+    xyz.x += cmf_x * phi_value;
+    xyz.y += cmf_y * phi_value;
+    xyz.z += cmf_z * phi_value;
+  }
+
+  return xyz;
+}
