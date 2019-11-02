086ff39e47cb33890ddc92fa7968d484db69984b
yumcyawiz <hashdolf1923@gmail.com>
2019-11-02 17:24:21 +0900

    add lambda to ray

--- src/ray.h
+++ src/ray.h
@@ -5,15 +5,15 @@
 #include "vec3.h"
 
 struct alignas(32) Ray {
-  Vec3 origin;
-  Vec3 direction;
+  Vec3 origin;     //始点
+  Vec3 direction;  //方向
+  Real lambda;     //波長
 
-  Ray() {};
-  Ray(const Vec3& _origin, const Vec3& _direction) : origin(_origin), direction(_direction) {};
+  Ray(){};
+  Ray(const Vec3& _origin, const Vec3& _direction)
+      : origin(_origin), direction(_direction){};
 
-  Vec3 operator()(const Real& t) const {
-    return origin + t*direction;
-  };
+  Vec3 operator()(const Real& t) const { return origin + t * direction; };
 };
 
 #endif
