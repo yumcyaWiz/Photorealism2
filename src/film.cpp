25b32073d8ae42629f54c2edc721d06325682f33
yumcyawiz <hashdolf1923@gmail.com>
2019-11-02 19:26:57 +0900

    fix ppm output

--- src/film.cpp
+++ src/film.cpp
@@ -15,15 +15,15 @@ void Film::writePPM(const std::string& filename) const {
 
       // RGBを計算
       const RGB rgb = spd.toRGB();
-      const unsigned char r = std::clamp(
-          static_cast<unsigned char>(255 * rgb.x),
-          static_cast<unsigned char>(0), static_cast<unsigned char>(255));
-      const unsigned char g = std::clamp(
-          static_cast<unsigned char>(255 * rgb.y),
-          static_cast<unsigned char>(0), static_cast<unsigned char>(255));
-      const unsigned char b = std::clamp(
-          static_cast<unsigned char>(255 * rgb.z),
-          static_cast<unsigned char>(0), static_cast<unsigned char>(255));
+      const unsigned int r = std::clamp(static_cast<unsigned int>(255 * rgb.x),
+                                        static_cast<unsigned int>(0),
+                                        static_cast<unsigned int>(255));
+      const unsigned int g = std::clamp(static_cast<unsigned int>(255 * rgb.y),
+                                        static_cast<unsigned int>(0),
+                                        static_cast<unsigned int>(255));
+      const unsigned int b = std::clamp(static_cast<unsigned int>(255 * rgb.z),
+                                        static_cast<unsigned int>(0),
+                                        static_cast<unsigned int>(255));
 
       //書き込み
       file << r << " " << g << " " << b << std::endl;
