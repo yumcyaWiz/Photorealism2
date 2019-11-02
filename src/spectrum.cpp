bfb85690792ea1ed3b7cfe8fbab1124e9b64a193
yumcyawiz <hashdolf1923@gmail.com>
2019-11-02 18:40:28 +0900

    split spectrum

---
+++ src/spectrum.cpp
@@ -0,0 +1,27 @@
+#include "spectrum.h"
+
+Real SPD::sample(const Real& l) const {
+  if (l < LAMBDA_MIN || l > LAMBDA_MAX) {
+    std::cerr << "lambda is out of bounds" << std::endl;
+    std::exit(EXIT_FAILURE);
+  } else {
+    std::size_t lambda2_idx =
+        std::lower_bound(lambda.begin(), lambda.end(), l) - lambda.begin();
+    std::size_t lambda1_idx = lambda2_idx - 1;
+
+    const Real t =
+        (l - lambda[lambda1_idx]) / (lambda[lambda2_idx] - lambda[lambda1_idx]);
+    return (1 - t) * phi[lambda1_idx] + t * phi[lambda2_idx];
+  }
+};
+
+void SPD::addSample(const Real& _l, const Real& _phi) {
+  std::size_t insert_idx =
+      std::lower_bound(lambda.begin(), lambda.end(), _l) - lambda.begin();
+  if (lambda[insert_idx] == _l) {
+    phi[insert_idx] = _phi;
+  } else {
+    lambda.insert(lambda.begin() + insert_idx - 1, _l);
+    phi.insert(phi.begin() + insert_idx - 1, _phi);
+  }
+}
--- src/spectrum.h
+++ src/spectrum.h
@@ -20,19 +20,10 @@ class SPD {
   constexpr static Real LAMBDA_MIN = 380;
   constexpr static Real LAMBDA_MAX = 780;
 
-  // 等色関数
-  static SPD color_matching_func_x;
-  static SPD color_matching_func_y;
-  static SPD color_matching_func_z;
-
   std::vector<Real> lambda;  //波長
   std::vector<Real> phi;     //放射束
 
-  SPD() : lambda({LAMBDA_MIN, LAMBDA_MAX}), phi({0, 0}) {
-    std::ifstream color_matching_func_file("ciexyz31.csv");
-
-    color_matching_func_file.close();
-  };
+  SPD() : lambda({LAMBDA_MIN, LAMBDA_MAX}), phi({0, 0}){};
   SPD(const std::vector<Real>& _lambda, const std::vector<Real>& _phi)
       : lambda(_lambda), phi(_phi){};
 
@@ -41,36 +32,14 @@ class SPD {
 
   //指定した波長の放射束を線形補間して返す
   // l : 波長[nm]
-  Real sample(const Real& l) const {
-    if (l < LAMBDA_MIN || l > LAMBDA_MAX) {
-      std::cerr << "lambda is out of bounds" << std::endl;
-      std::exit(EXIT_FAILURE);
-    } else {
-      std::size_t lambda2_idx =
-          std::lower_bound(lambda.begin(), lambda.end(), l) - lambda.begin();
-      std::size_t lambda1_idx = lambda2_idx - 1;
-
-      const Real t = (l - lambda[lambda1_idx]) /
-                     (lambda[lambda2_idx] - lambda[lambda1_idx]);
-      return (1 - t) * phi[lambda1_idx] + t * phi[lambda2_idx];
-    }
-  };
+  Real sample(const Real& l) const;
 
   //サンプルを追加する
-  void addSample(const Real& _l, const Real& _phi) {
-    std::size_t insert_idx =
-        std::lower_bound(lambda.begin(), lambda.end(), _l) - lambda.begin();
-    if (lambda[insert_idx] == _l) {
-      phi[insert_idx] = _phi;
-    } else {
-      lambda.insert(lambda.begin() + insert_idx - 1, _l);
-      phi.insert(phi.begin() + insert_idx - 1, _phi);
-    }
-  };
+  void addSample(const Real& _l, const Real& _phi);
 
   // XYZ色空間に変換する
   // http://cvrl.ucl.ac.uk/cmfs.htm
-  XYZ toXYZ() const {};
+  XYZ toXYZ() const;
 
   // sRGB色空間に変換する
   // XYZ to sRGB(D65)
