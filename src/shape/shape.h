89b9f70d6afe441b8fa7ca317d1ee3963c025ff2
yumcyawiz <hashdolf1923@gmail.com>
2019-11-04 22:25:27 +0900

    remove transform from shape

--- src/shape/shape.h
+++ src/shape/shape.h
@@ -1,11 +1,19 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+#include <memory>
+
 #include "isect.h"
 #include "ray.h"
+#include "transform.h"
 
+//物体の形状を表現するクラス
+//衝突計算は物体中心を原点とするローカル座標で行われる
 class Shape {
  public:
+  Shape(){};
+
+  //受け取ったレイとの衝突計算を行い、結果をinfoに格納する
   virtual bool intersect(const Ray& ray, IntersectInfo& info) const = 0;
 };
 
---
+++ src/shape/sphere.h
@@ -0,0 +1,11 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "shape/shape.h"
+#include "transform.h"
+#include "vec3.h"
+
+class Sphere {
+ public:
+};
+#endif
