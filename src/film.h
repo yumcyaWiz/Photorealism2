2903862634d5c016ce0e427f9a136c6291e6e0e1
yumcyawiz <hashdolf1923@gmail.com>
2019-11-04 10:39:09 +0900

    filmの分割コンパイル

--- src/film.h
+++ src/film.h
@@ -2,12 +2,10 @@
 #define FILM_H
 
 #include <cassert>
-#include <fstream>
-#include <iostream>
+#include <string>
 #include <vector>
 
 #include "spectrum.h"
-#include "vec3.h"
 
 class Film {
  public:
@@ -15,23 +13,16 @@ class Film {
   std::size_t height;       //縦幅
   std::vector<SPD> pixels;  //画素
 
-  Film(const std::size_t& _width, const std::size_t& _height)
-      : width(_width), height(_height) {
-    pixels.resize(width * height);
-  };
+  Film(const std::size_t& _width, const std::size_t& _height);
 
   // (i, j)のSPDを入手
-  SPD getPixel(int i, int j) const {
-    assert(i >= 0 && i < static_cast<int>(width));
-    assert(j >= 0 && j < static_cast<int>(height));
-    return pixels[i + width * j];
-  };
+  SPD getPixel(int i, int j) const;
 
   //(i, j)にSPDをセット
-  void setPixel(int i, int j, const SPD& spd) { pixels[i + width * j] = spd; };
+  void setPixel(int i, int j, const SPD& spd);
 
   //(i, j)にSPDを加算
-  void addPixel(int i, int j, const SPD& spd) { pixels[i + width * j] += spd; };
+  void addPixel(int i, int j, const SPD& spd);
 
   // PPMを出力
   void writePPM(const std::string& filename) const;
