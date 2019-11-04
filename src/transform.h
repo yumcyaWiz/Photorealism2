ac23b67ea11318a94e4c555a9dddc59e2ff9e0fb
yumcyawiz <hashdolf1923@gmail.com>
2019-11-05 01:29:29 +0900

    transformにコメント追加

--- src/transform.h
+++ src/transform.h
@@ -4,10 +4,11 @@
 #include "mat4.h"
 #include "vec3.h"
 
+//アフィン変換を行うクラス
 class Transform {
  public:
-  Mat4 mat;
-  Mat4 invmat;
+  Mat4 mat;     //変換行列
+  Mat4 invmat;  //逆変換行列
 
   Transform() : mat(identity()), invmat(identity()){};
   Transform(const Mat4& _mat, const Mat4& _invmat)
@@ -17,6 +18,7 @@ class Transform {
     return Transform(mat * t.mat, invmat * t.invmat);
   };
 
+  //方向ベクトルに対して変換を施す
   Vec3 applyDirection(const Vec3& v) const {
     return Vec3(
         mat.m[0][0] + v.x() + mat.m[0][1] * v.y() + mat.m[0][2] * v.z(),
@@ -24,6 +26,7 @@ class Transform {
         mat.m[2][0] * v.x() + mat.m[2][1] * v.y() + mat.m[2][2] * v.z());
   };
 
+  //点ベクトルに対して変換を施す
   Vec3 applyPoint(const Vec3& v) const {
     const Real x = mat.m[0][0] * v.x() + mat.m[0][1] * v.y() +
                    mat.m[0][2] * v.z() + mat.m[0][3];
@@ -36,6 +39,7 @@ class Transform {
     return Vec3(x / w, y / w, z / w);
   };
 
+  //法線ベクトルに対して変換を施す
   Vec3 applyNormal(const Vec3& n) const {
     return Vec3(invmat.m[0][0] * n.x() + invmat.m[1][0] * n.y() +
                     invmat.m[2][0] * n.z(),
@@ -46,12 +50,14 @@ class Transform {
   };
 };
 
+//移動変換を表す行列を返す
 Transform translate(const Vec3& v) {
   return Transform(
       Mat4(1, 0, 0, v.x(), 0, 1, 0, v.y(), 0, 0, 1, v.z(), 0, 0, 0, 1),
       Mat4(1, 0, 0, -v.x(), 0, 1, 0, -v.y(), 0, 0, 1, -v.z(), 0, 0, 0, 1));
 }
 
+//スケール変換を表す行列を返す
 Transform scale(const Vec3& s) {
   return Transform(
       Mat4(s.x(), 0, 0, 0, 0, s.y(), 0, 0, 0, 0, s.z(), 0, 0, 0, 0, 1),
@@ -59,6 +65,7 @@ Transform scale(const Vec3& s) {
            1));
 }
 
+// X軸中心の回転変換を表す行列を返す
 Transform rotateX(const Real& theta) {
   const Real c = std::cos(theta);
   const Real s = std::sin(theta);
@@ -66,6 +73,7 @@ Transform rotateX(const Real& theta) {
   return Transform(m, transpose(m));
 }
 
+// Y軸中心の回転変換を表す行列を返す
 Transform rotateY(const Real& theta) {
   const Real c = std::cos(theta);
   const Real s = std::sin(theta);
@@ -73,6 +81,7 @@ Transform rotateY(const Real& theta) {
   return Transform(m, transpose(m));
 }
 
+// Z軸中心の回転変換を表す行列を返す
 Transform rotateZ(const Real& theta) {
   const Real c = std::cos(theta);
   const Real s = std::sin(theta);
@@ -80,9 +89,11 @@ Transform rotateZ(const Real& theta) {
   return Transform(m, transpose(m));
 }
 
+// それぞれの軸をr回転する変換を表す行列を返す
 Transform rotate(const Vec3& r) {
   return rotateX(r.x()) * rotateY(r.y()) * rotateZ(r.z());
 }
 
+// 与えられたTransformの逆変換を表すTransformを返す
 Transform inverse(const Transform& t) { return Transform(t.invmat, t.mat); }
 #endif
