d77ae06a28d6e4df15733e1de41ebe74b9698a93
yumcyawiz <hashdolf1923@gmail.com>
2019-11-06 23:10:44 +0900

    compute pinhole distance from fov

--- prl2/src/camera/pinhole.h
+++ prl2/src/camera/pinhole.h
@@ -11,13 +11,15 @@ class PinholeCamera : public Camera {
   PinholeCamera(const std::shared_ptr<Film>& _film,
                 const std::shared_ptr<Transform>& _localToWorld,
                 const Real& _fov)
-      : Camera(_film, _localToWorld), fov(_fov), pinhole_distance(1){};
+      : Camera(_film, _localToWorld), fov(_fov) {
+    pinhole_distance = 0.5f * film->height_length / std::tan(0.5f * fov);
+  };
 
   bool generateRay(const Real& u, const Real& v, Ray& ray) const override;
 
  private:
-  const Real fov;               //横視野角
-  const Real pinhole_distance;  //ピンホールまでの距離
+  const Real fov;         //横視野角
+  Real pinhole_distance;  //ピンホールまでの距離
 };
 
 }  // namespace Prl2
