79a4263041962f50a6f0dfadb05277800d0c1f95
yumcyawiz <hashdolf1923@gmail.com>
2019-11-06 14:31:59 +0900

    move spectrum draw example to prl2 main

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -15,18 +15,6 @@ static void glfw_error_callback(int error, const char* desc) {
 }
 
 int main() {
-  Prl2::Film film(512, 512);
-  for (std::size_t i = 0; i < film.width; ++i) {
-    for (std::size_t j = 0; j < film.height; ++j) {
-      const Prl2::Real l = float(i) / film.width * (780 - 380) + 380;
-      std::vector<Prl2::Real> lambda{l - 5, l, l + 5};
-      std::vector<Prl2::Real> phi{0, 1, 0};
-      Prl2::SPD spd(lambda, phi);
-      film.addPixel(i, j, spd);
-    }
-  }
-  film.writePPM("output.ppm");
-
   // GLFWを初期化
   glfwSetErrorCallback(glfw_error_callback);
   if (!glfwInit()) {
