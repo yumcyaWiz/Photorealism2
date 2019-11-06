66a032f220b636cea6c962334c520cbe4f54cbfc
yumcyawiz <hashdolf1923@gmail.com>
2019-11-06 14:45:43 +0900

    test intersect

--- prl2/main.cpp
+++ prl2/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <memory>
 
 #include "prl2.h"
 
+using namespace Prl2;
+
 int main() {
+  /*
   Prl2::Film film(512, 512);
   for (std::size_t i = 0; i < film.width; ++i) {
     for (std::size_t j = 0; j < film.height; ++j) {
@@ -14,6 +18,17 @@ int main() {
     }
   }
   film.writePPM("output.ppm");
+  */
+  auto shape = std::make_shared<Sphere>(1.0f);
+  auto transform = std::make_shared<Transform>(translate(Vec3(0, 0, 3)));
+  auto prim = std::make_shared<Primitive>(shape, transform);
+
+  const Ray ray(Vec3(0, 0, 0), Vec3(0, 0, 1), 0);
+  IntersectInfo info;
+
+  if (prim->intersect(ray, info)) {
+    std::cout << info << std::endl;
+  }
 
   return 0;
 }
--- prl2/src/core/isect.h
+++ prl2/src/core/isect.h
@@ -1,6 +1,8 @@
 #ifndef ISECT_H
 #define ISECT_H
 
+#include <iostream>
+
 #include "core/ray.h"
 #include "core/type.h"
 #include "core/vec3.h"
@@ -16,6 +18,14 @@ struct IntersectInfo {
   IntersectInfo(const Real& _t, const Vec3& _hitPos, const Vec3& _hitNormal)
       : t(_t), hitPos(_hitPos), hitNormal(_hitNormal){};
 };
+
+inline std::ostream& operator<<(std::ostream& stream,
+                                const IntersectInfo& info) {
+  stream << "t: " << info.t << std::endl;
+  stream << "hitPos: " << info.hitPos << std::endl;
+  stream << "hitNormal: " << info.hitNormal << std::endl;
+  return stream;
+}
 
 }  // namespace Prl2
 
