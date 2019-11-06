cb83ffc3808846fa97574e25d99217f48986cbce
yumcyawiz <hashdolf1923@gmail.com>
2019-11-06 22:44:53 +0900

    add ray generation assert

--- prl2/src/camera/pinhole.cpp
+++ prl2/src/camera/pinhole.cpp
@@ -3,8 +3,12 @@
 namespace Prl2 {
 
 bool PinholeCamera::generateRay(const Real& u, const Real& v, Ray& ray) const {
+  assert(u >= -1 && u <= 1);
+  assert(v >= -1 && v <= 1);
+
   Vec2 pFilm_2d = film->computePosition(u, v);
   Vec3 pFilm(pFilm_2d.x(), pFilm_2d.y(), 0);
+  std::cout << pFilm << std::endl;
   Vec3 pinholePos = Vec3(0, 0, -pinhole_distance);
 
   //ローカル座標系のレイ
