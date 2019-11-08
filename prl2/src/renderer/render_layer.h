e523ef5e4c9de487c9f81c0ff1c2964d1fa89e2a
yumcyawiz <hashdolf1923@gmail.com>
2019-11-08 12:34:06 +0900

    render layer init

--- prl2/src/renderer/render_layer.h
+++ prl2/src/renderer/render_layer.h
@@ -4,17 +4,25 @@
 #include <vector>
 
 #include "core/type.h"
+#include "renderer/render_config.h"
 
 namespace Prl2 {
 
 //レンダリング後の画像を格納する
 struct RenderLayer {
-  RenderLayer(){};
+  // RenderConfigから画像を初期化
+  RenderLayer(const RenderConfig& config) {
+    render_sRGB.resize(config.width * config.height, 0);
+    normal_sRGB.resize(config.width * config.height, 0);
+    depth_sRGB.resize(config.width * config.height, 0);
+    position_sRGB.resize(config.width * config.height, 0);
+  };
 
-  std::vector<Real> sRGB;  // レンダリング結果のsRGBを[0, 1]の範囲で格納する
-  std::vector<Real> normalsRGB;  // 法線をsRGBにしたものを格納する
-  std::vector<Real> depthsRGB;   //深度をsRGBにしたものを格納する
-  std::vector<Real> positionsRGB;  //位置をsRGBにしたものを格納する
+  std::vector<Real>
+      render_sRGB;  // レンダリング結果のsRGBを[0, 1]の範囲で格納する
+  std::vector<Real> normal_sRGB;  // 法線をsRGBにしたものを格納する
+  std::vector<Real> depth_sRGB;   //深度をsRGBにしたものを格納する
+  std::vector<Real> position_sRGB;  //位置をsRGBにしたものを格納する
 };
 
 }  // namespace Prl2
---
+++ prl2/src/renderer/renderer.cpp
@@ -0,0 +1,8 @@
+#include "renderer/renderer.h"
+
+namespace Prl2 {
+
+void Renderer::render(const Scene& scene, const RenderConfig& config,
+                      RenderLayer& layer) const {}
+
+};  // namespace Prl2
