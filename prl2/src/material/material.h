4aeb4a3506cb203410d70cbdeaf0c92a4f1674d0
yumcyawiz <hashdolf1923@gmail.com>
2019-11-09 15:42:14 +0900

    add material

--- prl2/src/material/material.h
+++ prl2/src/material/material.h
@@ -1,9 +1,22 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include "core/isect.h"
+#include "core/ray.h"
+#include "core/type.h"
+
 namespace Prl2 {
 
-class Material {};
+// Materialを表現するクラス
+// Materialにおいて方向ベクトルは原点を衝突点、+Yを法線とする座標系で定義される
+class Material {
+  Material(){};
+
+  //次のレイの方向をサンプリングする
+  //評価した分光反射率を返り値とする
+  virtual Real sampleDirection(const Ray& ray_in, const IntersectInfo& info,
+                               Ray& ray_out) const = 0;
+};
 
 }  // namespace Prl2
 
