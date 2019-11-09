04dc4af59cf4e7eeab47115b5667d9ab931d9d74
yumcyawiz <hashdolf1923@gmail.com>
2019-11-09 15:53:15 +0900

    add diffuse

---
+++ prl2/src/material/diffuse.h
@@ -0,0 +1,22 @@
+#ifndef DIFFUSE_H
+#define DIFFUSE_H
+
+#include "core/spectrum.h"
+#include "material/material.h"
+
+namespace Prl2 {
+
+class Diffuse : public Material {
+ public:
+  Diffuse(const SPD& _albedo) : albedo(_albedo){};
+
+  Real sampleDirection(const Ray& ray, const IntersectInfo& info, Ray& ray_out,
+                       Real& pdf) const {};
+
+ private:
+  SPD albedo;
+};
+
+}  // namespace Prl2
+
+#endif
--- prl2/src/material/material.h
+++ prl2/src/material/material.h
@@ -10,12 +10,13 @@ namespace Prl2 {
 // Materialを表現するクラス
 // Materialにおいて方向ベクトルは原点を衝突点、+Yを法線とする座標系で定義される
 class Material {
+ public:
   Material(){};
 
   //次のレイの方向をサンプリングする
   //評価した分光反射率を返り値とする
   virtual Real sampleDirection(const Ray& ray_in, const IntersectInfo& info,
-                               Ray& ray_out) const = 0;
+                               Ray& ray_out, Real& pdf) const = 0;
 };
 
 }  // namespace Prl2
