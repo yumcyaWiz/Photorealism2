14bdf4ff269228004cd6fc9d905f3ede8cb61a2d
yumcyawiz <hashdolf1923@gmail.com>
2019-11-09 12:28:31 +0900

    add draw normal test

---
+++ tests/draw_normal.cpp
@@ -0,0 +1,14 @@
+#include "camera/film.h"
+#include "camera/pinhole.h"
+#include "core/primitive.h"
+#include "renderer/renderer.h"
+
+using namespace Prl2;
+
+int main() {
+  RenderConfig config;
+
+  RenderLayer layer(config);
+
+  Renderer renderer;
+}
