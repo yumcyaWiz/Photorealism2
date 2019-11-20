5638ca82972aef985953bd79954f90eff03a4518
yumcyawiz <hashdolf1923@gmail.com>
2019-11-20 17:05:51 +0900

    add constant

---
+++ prl2/src/core/constant.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+#include <cmath>
+
+#include "core/type.h"
+
+namespace Prl2 {
+
+static constexpr Real PI = M_PI;
+static constexpr Real PI_MUL_2 = M_PI * 2;
+static constexpr Real PI_DIV_2 = M_PI / 2;
+
+}  // namespace Prl2
+
+#endif
