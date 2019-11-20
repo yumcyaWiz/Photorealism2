8967ff6016a8fd097073228363c8f599ff5cbf91
yumcyawiz <hashdolf1923@gmail.com>
2019-11-20 17:33:30 +0900

    add inv pi constant

--- prl2/src/core/constant.h
+++ prl2/src/core/constant.h
@@ -11,6 +11,9 @@ static constexpr Real PI = M_PI;
 static constexpr Real PI_MUL_2 = M_PI * 2;
 static constexpr Real PI_DIV_2 = M_PI / 2;
 
+static constexpr Real INV_PI = M_PI;
+static constexpr Real INV_PI_MUL_2 = 1 / PI_MUL_2;
+
 }  // namespace Prl2
 
 #endif
