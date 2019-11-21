f9ca890dbc369f2b646596dd77899cdbce6c6a82
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 11:25:45 +0900

    fix pi definitions

--- prl2/src/core/constant.h
+++ prl2/src/core/constant.h
@@ -7,11 +7,11 @@
 
 namespace Prl2 {
 
-static constexpr Real PI = M_PI;
-static constexpr Real PI_MUL_2 = M_PI * 2;
-static constexpr Real PI_DIV_2 = M_PI / 2;
+static constexpr Real PI = 3.141592653589793238462643383279;
+static constexpr Real PI_MUL_2 = PI * 2;
+static constexpr Real PI_DIV_2 = PI / 2;
 
-static constexpr Real INV_PI = M_PI;
+static constexpr Real INV_PI = 1 / PI;
 static constexpr Real INV_PI_MUL_2 = 1 / PI_MUL_2;
 
 }  // namespace Prl2
