b3948db0ecb08c1b7f0e6eee3dcc77e517c1167f
yumcyawiz <hashdolf1923@gmail.com>
2019-11-22 07:33:56 +0900

    fix tonemapping

--- app/src/tonemapping.h
+++ app/src/tonemapping.h
@@ -4,6 +4,39 @@
 #include <cmath>
 #include <vector>
 
+// 露光調整を行う
+void adjustExposure(int width, int height, const std::vector<float>& rgb_in,
+                    float exposure, std::vector<float>& rgb_out) {
+  // 平均輝度を計算
+  float mean = 0;
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      mean += std::sqrt(r * r + g * g + b * b);
+    }
+  }
+  mean /= (width * height);
+
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      rgb_out[index + 0] = r * exposure / mean;
+      rgb_out[index + 1] = g * exposure / mean;
+      rgb_out[index + 2] = b * exposure / mean;
+    }
+  }
+}
+
 // ガンマ補正を行う
 void gammaCorrection(int width, int height, const std::vector<float>& rgb_in,
                      float gamma, std::vector<float>& rgb_out) {
@@ -14,10 +47,11 @@ void gammaCorrection(int width, int height, const std::vector<float>& rgb_in,
       const float r = rgb_in[index + 0];
       const float g = rgb_in[index + 1];
       const float b = rgb_in[index + 2];
+      const float intensity = std::sqrt(r * r + g * g + b * b);
 
-      rgb_out[index + 0] = std::pow(r, 1 / gamma);
-      rgb_out[index + 1] = std::pow(g, 1 / gamma);
-      rgb_out[index + 2] = std::pow(b, 1 / gamma);
+      rgb_out[index + 0] = r * std::pow(intensity, 1 / gamma);
+      rgb_out[index + 1] = g * std::pow(intensity, 1 / gamma);
+      rgb_out[index + 2] = b * std::pow(intensity, 1 / gamma);
     }
   }
 }
@@ -52,9 +86,11 @@ void reinHardToneMapping(int width, int height,
       const float g = rgb_in[index + 1];
       const float b = rgb_in[index + 2];
 
-      rgb_out[index + 0] = r / (1 + r);
-      rgb_out[index + 1] = g / (1 + g);
-      rgb_out[index + 2] = b / (1 + b);
+      const float intensity = std::sqrt(r * r + g * g + b * b);
+
+      rgb_out[index + 0] = r * intensity / (1 + intensity);
+      rgb_out[index + 1] = g * intensity / (1 + intensity);
+      rgb_out[index + 2] = b * intensity / (1 + intensity);
     }
   }
 }
