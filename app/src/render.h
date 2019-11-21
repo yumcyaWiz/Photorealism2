dab8efec34362aa5fcf7ef293493b2d82ec43320
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 13:10:22 +0900

    make static

--- app/src/render.h
+++ app/src/render.h
@@ -2,6 +2,8 @@
 #define _APP_RENDER_H
 
 #include <atomic>
+#include <chrono>
+#include <thread>
 
 #include "renderer/renderer.h"
 
@@ -14,15 +16,45 @@ class Render {
   };
 
   // シーンを初期化する
-  void initScene(const Prl2::RenderConfig& config);
+  static void initScene(const Prl2::RenderConfig& config);
+
+  // レンダリングする
+  static void requestRender() {
+    if (refreshRender) {
+      cancelRender = true;
+    }
+    refreshRender = true;
+  };
+
+  // レンダリングループ
+  static void render() {
+    while (true) {
+      if (refreshRender) {
+        const auto start_time = std::chrono::system_clock::now();
+        renderer.render(layer);
+
+        if (cancelRender) {
+          cancelRender = false;
+        } else {
+          std::cout << "Rendering Finished in "
+                    << std::chrono::duration_cast<std::chrono::milliseconds>(
+                           std::chrono::system_clock::now() - start_time)
+                           .count()
+                    << "ms" << std::endl;
+          refreshRender = false;
+        }
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+  };
 
  private:
-  Prl2::RenderConfig config;  // RenderConfig
-  Prl2::RenderLayer layer;    // RenderLayer
-  Prl2::Renderer renderer;    // Renderer
+  static Prl2::RenderConfig config;  // RenderConfig
+  static Prl2::RenderLayer layer;    // RenderLayer
+  static Prl2::Renderer renderer;    // Renderer
 
-  std::atomic<bool> cancelRender;  // レンダラーのキャンセルフラグ
-  std::atomic<bool> refreshRender;  // レンダラーの再レンダリングフラグ
+  static std::atomic<bool> cancelRender;  // レンダラーのキャンセルフラグ
+  static std::atomic<bool> refreshRender;  // レンダラーの再レンダリングフラグ
 };
 
 #endif
