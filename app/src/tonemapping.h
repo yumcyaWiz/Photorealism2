01c143821487c170577749b4f2becdf9552a03a1
yumcyawiz <hashdolf1923@gmail.com>
2019-11-22 06:47:12 +0900

    impl reinhard tonemap

--- app/src/tonemapping.h
+++ app/src/tonemapping.h
@@ -22,4 +22,22 @@ void gammaCorrection(int width, int height, const std::vector<float>& rgb_in,
   }
 }
 
+// Reinhard Tone Mapping
+void reinHardToneMap(int width, int height, const std::vector<float>& rgb_in,
+                     std::vector<float>& rgb_out) {
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      rgb_out[index + 0] = r / (1 + r);
+      rgb_out[index + 1] = g / (1 + g);
+      rgb_out[index + 2] = b / (1 + b);
+    }
+  }
+}
+
 #endif
