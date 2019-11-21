0177b686e8567719b26ac5cee340097f72d615b3
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 20:59:12 +0900

    fix parallel 2d

--- lib/prl2/src/parallel/parallel.h
+++ lib/prl2/src/parallel/parallel.h
@@ -37,17 +37,23 @@ inline void parallelFor1D(const std::function<void(int)>& job, int nChunks,
   }
 }
 
+// 二重For文を並列実行する
+// job: 並列化する対象の関数
+// nChunks_x: X方向のループ分割数
+// nChunks_y: Y方向のループ分割数
+// nx: X方向のループ数
+// ny: Y方向のループ数
 inline void parallelFor2D(const std::function<void(int, int)>& job,
-                          int chunkSize_x, int chunkSize_y, int nx, int ny) {
+                          int nChunks_x, int nChunks_y, int nx, int ny) {
   const int num_threads = std::max(1U, std::thread::hardware_concurrency());
 
   ThreadPool pool(num_threads);
   std::vector<std::future<void>> results;
 
-  const int nChunks_x = nx / chunkSize_x;
-  const int nChunks_y = ny / chunkSize_y;
-  for (int chunk_x = 0; chunk_x < nChunks_x; ++chunk_x) {
-    for (int chunk_y = 0; chunk_y < nChunks_y; ++chunk_y) {
+  const int chunkSize_x = nx / nChunks_x;
+  const int chunkSize_y = ny / nChunks_y;
+  for (int chunk_x = 0; chunk_x < chunkSize_x; ++chunk_x) {
+    for (int chunk_y = 0; chunk_y < chunkSize_y; ++chunk_y) {
       results.push_back(
           pool.enqueue([chunk_x, chunk_y, chunkSize_x, chunkSize_y, job] {
             const int start_x = chunk_x * chunkSize_x;
