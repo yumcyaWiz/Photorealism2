635e948da1cab3aafc9274c18276ed9da02f461e
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 20:13:15 +0900

    impl parallelFor1D

--- lib/prl2/src/parallel/parallel.h
+++ lib/prl2/src/parallel/parallel.h
@@ -3,18 +3,30 @@
 
 #include <algorithm>
 #include <functional>
+#include <future>
 #include <thread>
 
 #include "ThreadPool/ThreadPool.h"
 
 namespace Prl2 {
 
-template <class R, class... ArgTypes>
-void parallelFor1D(const std::function<R(ArgTypes...)>& job) {
+void parallelFor1D(const std::function<void(int)>& job, int chunkSize, int n) {
   const int num_threads = std::max(1U, std::thread::hardware_concurrency());
 
   ThreadPool pool(num_threads);
-}  // namespace Prl2
+  std::vector<std::future<void>> results;
+
+  const int nChunks = n / chunkSize;
+  for (int chunk_id = 0; chunk_id < nChunks; ++chunk_id) {
+    results.push_back(pool.enqueue([&] {
+      const int start_i = chunk_id * chunkSize;
+      const int end_i = (chunk_id + 1) * chunkSize;
+      for (int i = start_i; i < end_i; ++i) {
+        job(i);
+      }
+    }));
+  }
+}
 
 }  // namespace Prl2
 
