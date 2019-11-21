07fc19128bff77f69824ced21aa5e488aa29b57f
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 15:22:45 +0900

    make texture from rgb array

--- app/src/gui.cpp
+++ app/src/gui.cpp
@@ -1,3 +1,22 @@
 #include "gui.h"
+#include "imgui.h"
 
-void GUI::drawRenderLayer() const { return; }
+void GUI::drawRenderLayer() const {
+  ImGui::Begin("RenderLayer");
+  {
+    ImTextureID id = (ImTextureID)(intptr_t)(normal_texture_id);
+    ImGui::Image(id, ImVec2(width, height));
+  }
+  ImGui::End();
+}
+
+void GUI::makeTextureFromRGB(GLuint texture_id, int width, int height,
+                             const std::vector<float>& rgb) {
+  glBindTexture(GL_TEXTURE_2D, texture_id);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_FLOAT,
+               rgb.data());
+}
--- app/src/gui.h
+++ app/src/gui.h
@@ -2,25 +2,35 @@
 #define GUI_H
 
 #include "GLFW/glfw3.h"
+#include "render.h"
+#include "renderer/render_layer.h"
 
 class GUI {
  public:
   GUI() {
     // Textureの用意
-    glGenTextures(1, &render_texture);
-    glGenTextures(1, &normal_texture);
-    glGenTextures(1, &position_texture);
-    glGenTextures(1, &depth_texture);
+    glGenTextures(1, &render_texture_id);
+    glGenTextures(1, &normal_texture_id);
+    glGenTextures(1, &position_texture_id);
+    glGenTextures(1, &depth_texture_id);
   };
+
+  // RenderLayer Textureを更新
+  void updateRenderLayerTexture(const Prl2::RenderLayer& layer);
 
   // RenderLayerを表示
   void drawRenderLayer() const;
 
  private:
-  GLuint render_texture;    // Render Texture
-  GLuint normal_texture;    // Normal Texture
-  GLuint position_texture;  // Position Texture
-  GLuint depth_texture;     // Depth Texture
+  int width;
+  int height;
+  GLuint render_texture_id;    // Render Texture
+  GLuint normal_texture_id;    // Normal Texture
+  GLuint position_texture_id;  // Position Texture
+  GLuint depth_texture_id;     // Depth Texture
+
+  void makeTextureFromRGB(GLuint texture_id, int width, int height,
+                          const std::vector<float>& rgb);
 };
 
 #endif
