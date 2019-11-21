887ef415b718b865b30c65a4974145c2423cfed7
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 19:10:22 +0900

    take functional

--- lib/prl2/src/parallel/parallel.h
+++ lib/prl2/src/parallel/parallel.h
@@ -2,17 +2,19 @@
 #define _PRL2_PARALLEL_H
 
 #include <algorithm>
+#include <functional>
 #include <thread>
 
 #include "ThreadPool/ThreadPool.h"
 
 namespace Prl2 {
 
-void parallelFor1D() {
+template <class R, class... ArgTypes>
+void parallelFor1D(const std::function<R(ArgTypes...)>& job) {
   const int num_threads = std::max(1U, std::thread::hardware_concurrency());
 
   ThreadPool pool(num_threads);
-}
+}  // namespace Prl2
 
 }  // namespace Prl2
 
