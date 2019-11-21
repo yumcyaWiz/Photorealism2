697bcef1658ff70b403b820c823540394718eaab
yumcyawiz <hashdolf1923@gmail.com>
2019-11-21 15:06:37 +0900

    add gui

--- app/src/gui.h
+++ app/src/gui.h
@@ -1,9 +1,23 @@
 #ifndef GUI_H
 #define GUI_H
 
+#include "GLFW/glfw3.h"
+
 class GUI {
  public:
-  GUI(){};
+  GUI() {
+    // Textureの用意
+    glGenTextures(1, &render_texture);
+    glGenTextures(1, &normal_texture);
+    glGenTextures(1, &position_texture);
+    glGenTextures(1, &depth_texture);
+  };
+
+ private:
+  GLuint render_texture;    // Render Texture
+  GLuint normal_texture;    // Normal Texture
+  GLuint position_texture;  // Position Texture
+  GLuint depth_texture;     // Depth Texture
 };
 
 #endif
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -8,6 +8,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+#include "gui.h"
 #include "render.h"
 
 static void glfw_error_callback(int error, const char* desc) {
