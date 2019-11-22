318feed23573d27a7f76990c858a5d4473443828
yumcyawiz <hashdolf1923@gmail.com>
2019-11-22 12:35:03 +0900

    move tonemapping to postprocess

---
+++ lib/prl2/src/postprocess/tone_mapping.h
@@ -0,0 +1,104 @@
+#ifndef _PRL2_TONE_MAPPING_H
+#define _PRL2_TONE_MAPPING_H
+
+#include <cmath>
+#include <vector>
+
+namespace Prl2 {
+
+// TODO: XYZ色空間で処理を行う
+
+// 露光調整を行う
+void adjustExposure(int width, int height, const std::vector<float>& rgb_in,
+                    float exposure, std::vector<float>& rgb_out) {
+  // 平均輝度を計算
+  float mean = 0;
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      mean += std::sqrt(r * r + g * g + b * b);
+    }
+  }
+  mean /= (width * height);
+
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      rgb_out[index + 0] = r * exposure / mean;
+      rgb_out[index + 1] = g * exposure / mean;
+      rgb_out[index + 2] = b * exposure / mean;
+    }
+  }
+}
+
+// ガンマ補正を行う
+void gammaCorrection(int width, int height, const std::vector<float>& rgb_in,
+                     float gamma, std::vector<float>& rgb_out) {
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+      const float intensity = std::sqrt(r * r + g * g + b * b);
+
+      rgb_out[index + 0] = r * std::pow(intensity, 1 / gamma);
+      rgb_out[index + 1] = g * std::pow(intensity, 1 / gamma);
+      rgb_out[index + 2] = b * std::pow(intensity, 1 / gamma);
+    }
+  }
+}
+
+// Linear Tone Mapping
+void linearToneMapping(int width, int height, const std::vector<float>& rgb_in,
+                       std::vector<float>& rgb_out) {
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      rgb_out[index + 0] = r;
+      rgb_out[index + 1] = g;
+      rgb_out[index + 2] = b;
+    }
+  }
+}
+
+// Reinhard Tone Mapping
+void reinHardToneMapping(int width, int height,
+                         const std::vector<float>& rgb_in,
+                         std::vector<float>& rgb_out) {
+  for (int j = 0; j < height; ++j) {
+    for (int i = 0; i < width; ++i) {
+      const int index = 3 * i + 3 * width * j;
+
+      const float r = rgb_in[index + 0];
+      const float g = rgb_in[index + 1];
+      const float b = rgb_in[index + 2];
+
+      const float intensity = std::sqrt(r * r + g * g + b * b);
+
+      rgb_out[index + 0] = r * intensity / (1 + intensity);
+      rgb_out[index + 1] = g * intensity / (1 + intensity);
+      rgb_out[index + 2] = b * intensity / (1 + intensity);
+    }
+  }
+}
+
+}  // namespace Prl2
+
+#endif
