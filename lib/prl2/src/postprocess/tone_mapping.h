e6cfbda333a6fe28ea21028cc0eb9ac5461fdf3b
yumcyawiz <hashdolf1923@gmail.com>
2019-11-22 14:36:04 +0900

    impl reinhard tonemap

--- lib/prl2/src/postprocess/tone_mapping.h
+++ lib/prl2/src/postprocess/tone_mapping.h
@@ -6,43 +6,9 @@
 
 namespace Prl2 {
 
-// Linear Tone Mapping
-void linearToneMapping(int width, int height, const std::vector<float>& xyz_in,
-                       std::vector<float>& xyz_out) {
-  for (int j = 0; j < height; ++j) {
-    for (int i = 0; i < width; ++i) {
-      const int index = 3 * i + 3 * width * j;
-
-      const float x = xyz_in[index + 0];
-      const float y = xyz_in[index + 1];
-      const float z = xyz_in[index + 2];
-
-      xyz_out[index + 0] = x;
-      xyz_out[index + 1] = y;
-      xyz_out[index + 2] = z;
-    }
-  }
-}
-
 // Reinhard Tone Mapping
-void reinHardToneMapping(int width, int height,
-                         const std::vector<float>& rgb_in,
-                         std::vector<float>& rgb_out) {
-  for (int j = 0; j < height; ++j) {
-    for (int i = 0; i < width; ++i) {
-      const int index = 3 * i + 3 * width * j;
-
-      const float r = rgb_in[index + 0];
-      const float g = rgb_in[index + 1];
-      const float b = rgb_in[index + 2];
-
-      const float intensity = std::sqrt(r * r + g * g + b * b);
-
-      rgb_out[index + 0] = r * intensity / (1 + intensity);
-      rgb_out[index + 1] = g * intensity / (1 + intensity);
-      rgb_out[index + 2] = b * intensity / (1 + intensity);
-    }
-  }
+float reinhardToneMapping(float luminance, float exposure) {
+  return luminance * (1 + luminance / (exposure * exposure)) / (1 + luminance);
 }
 
 }  // namespace Prl2
