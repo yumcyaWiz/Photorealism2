09134b7d96eaa2a1c66514b057cde6821705e37a
yumcyawiz <hashdolf1923@gmail.com>
2019-11-24 07:37:52 +0900

    impli hosek sky

--- lib/prl2/src/sky/hosek_sky.h
+++ lib/prl2/src/sky/hosek_sky.h
@@ -1,13 +1,38 @@
 #ifndef _PRL2_HOSEK_SKY
 #define _PRL2_HOSEK_SKY
 
+#include "HosekWilkie/ArHosekSkyModel.h"
+
+#include "core/spectrum.h"
 #include "sky/sky.h"
 
 namespace Prl2 {
 
 class HosekSky : public Sky {
  public:
-  HosekSky()
+  HosekSky(const Real& albedo, const Real& turbidity,
+           const Real& solarElevation) {
+    for (int i = 0; i < SPD::LAMBDA_SAMPLES; ++i) {
+      state[i] =
+          arhosekskymodelstate_alloc_init(turbidity, albedo, solarElevation);
+    }
+  };
+  ~HosekSky() {
+    for (int i = 0; i < SPD::LAMBDA_SAMPLES; ++i) {
+      arhosekskymodelstate_free(state[i]);
+    }
+  };
+
+  Real getRadiance(const Ray& ray) const override {
+    // Compute (theta, phi)
+    const Real theta;
+
+    // Compute State Index
+    return 0;
+  };
+
+ private:
+  ArHosekSkyModelState* state[SPD::LAMBDA_SAMPLES];
 };
 
 };  // namespace Prl2
