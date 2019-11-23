ca3a64656dee09b606138dd429a9bede91beefeb
yumcyawiz <hashdolf1923@gmail.com>
2019-11-24 07:01:42 +0900

    write cmake

---
+++ lib/prl2/src/sky/hosek_sky.h
@@ -0,0 +1,15 @@
+#ifndef _PRL2_HOSEK_SKY
+#define _PRL2_HOSEK_SKY
+
+#include "sky/sky.h"
+
+namespace Prl2 {
+
+class HosekSky : public Sky {
+ public:
+  HosekSky()
+};
+
+};  // namespace Prl2
+
+#endif
