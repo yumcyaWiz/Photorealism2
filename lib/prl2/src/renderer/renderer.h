d5e4e8f747009b89867457be3ef8ee92e3e1139e
yumcyawiz <hashdolf1923@gmail.com>
2019-11-23 14:24:53 +0900

    add getter

--- lib/prl2/src/renderer/renderer.h
+++ lib/prl2/src/renderer/renderer.h
@@ -32,6 +32,11 @@ class Renderer {
   void render(const std::atomic<bool>& cancel);
 
   // Render Settings
+  // 出力サイズを入手する
+  void getImageSize(int& sx, int& sy) const {
+    sx = config.width;
+    sy = config.height;
+  };
   // 出力サイズを変更する
   void setImageSize(unsigned int width, unsigned int height) {
     config.width = width;
@@ -40,14 +45,20 @@ class Renderer {
     scene.camera->film->resize(width, height);
   };
 
+  // サンプル数を入手する
+  unsigned int getSamples() const { return config.samples; };
   // サンプル数を設定する
   void setSamples(unsigned int samples) { config.samples = samples; }
 
+  // 出力レイヤーを入手する
+  LayerType getOutputLayer() const { return config.layer_type; };
   // 出力レイヤーを指定する
   void setOutputLayer(const LayerType& _layer_type) {
     config.layer_type = _layer_type;
   };
 
+  // 出力画像形式を入手する
+  ImageType getImageType() const { return config.image_type; };
   // 出力画像形式を指定する
   void setImageType(const ImageType& _image_type) {
     config.image_type = _image_type;
@@ -55,22 +66,36 @@ class Renderer {
 
   // Post Processing
   // 露光を設定
+  Real getExposure() const { return config.exposure; };
   void setExposure(const Real& exposure) { config.exposure = exposure; };
 
+  // ガンマ値を入手
+  Real getGamma() const { return config.gamma; };
   // ガンマ値を設定
   void setGamma(const Real& gamma) { config.gamma = gamma; };
 
+  // Tone Mappingの種類を入手
+  ToneMappingType getToneMappingType() const {
+    return config.tone_mapping_type;
+  };
   // Tone Mappingの種類を指定
   void setToneMappingType(const ToneMappingType& _tone_mapping_type) {
     config.tone_mapping_type = _tone_mapping_type;
   };
 
+  // Tone Mappingの種類を入手
+  Real getMappingFactor() const { return config.mapping_factor; };
   // Mapping Factorを指定
   void setMappingFactor(const Real& _mapping_factor) {
     config.mapping_factor = _mapping_factor;
   };
 
   // Film
+  // フィルムの長さを入手する
+  void getFilmLength(Real& lx, Real& ly) const {
+    lx = config.width_length;
+    ly = config.height_length;
+  };
   // フィルムの長さを設定する
   void setFilmLength(const Real& width_length, const Real& height_length) {
     config.width_length = width_length;
@@ -78,6 +103,11 @@ class Renderer {
     scene.camera->film->resizeLength(width_length, height_length);
   };
 
+  // カメラの位置、向きを入手する
+  void getCameraLookAt(Vec3& pos, Vec3& lookat) {
+    pos = config.camera_position;
+    lookat = config.camera_lookat;
+  };
   // カメラの位置、向きを設定する
   void setCameraLookAt(const Vec3& pos, const Vec3& lookat) {
     config.camera_position = pos;
