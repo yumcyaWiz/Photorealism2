f8033762f2fe37198231f232e6a56005225e7d12
yumcyawiz <hashdolf1923@gmail.com>
2019-11-24 13:18:10 +0900

    add plane

---
+++ lib/prl2/src/shape/plane.h
@@ -0,0 +1,17 @@
+#ifndef _PRL2_PLANE_H
+#define _PRL2_PLANE_H
+
+#include "shape/shape.h"
+
+namespace Prl2 {
+
+class Plane : public Shape {
+ public:
+  Plane(){};
+
+  bool intersect(const Ray& ray, IntersectInfo& info) const override{
+
+  };
+};
+
+}  // namespace Prl2
