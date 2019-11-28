eb347a5a2fa78daeafcce8114331fdb3fdebb025
yumcyawiz <hashdolf1923@gmail.com>
2019-11-29 04:07:52 +0900

    change parallel rendering direction

--- lib/prl2/src/parallel/parallel.h
+++ lib/prl2/src/parallel/parallel.h
@@ -52,16 +52,16 @@ inline void parallelFor2D(const std::function<void(int, int)>& job,
 
   const int chunkSize_x = nx / nChunks_x;
   const int chunkSize_y = ny / nChunks_y;
-  for (int chunk_x = 0; chunk_x < nChunks_x; ++chunk_x) {
-    for (int chunk_y = 0; chunk_y < nChunks_y; ++chunk_y) {
+  for (int chunk_y = 0; chunk_y < nChunks_y; ++chunk_y) {
+    for (int chunk_x = 0; chunk_x < nChunks_x; ++chunk_x) {
       results.push_back(
           pool.enqueue([chunk_x, chunk_y, chunkSize_x, chunkSize_y, job] {
             const int start_x = chunk_x * chunkSize_x;
             const int end_x = (chunk_x + 1) * chunkSize_x;
             const int start_y = chunk_y * chunkSize_y;
             const int end_y = (chunk_y + 1) * chunkSize_y;
-            for (int x = start_x; x < end_x; ++x) {
-              for (int y = start_y; y < end_y; ++y) {
+            for (int y = start_y; y < end_y; ++y) {
+              for (int x = start_x; x < end_x; ++x) {
                 job(x, y);
               }
             }
