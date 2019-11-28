fe4d335425ac16a0f430c61be89f647ea691a47a
yumcyawiz <hashdolf1923@gmail.com>
2019-11-29 04:31:32 +0900

    separate compile hosek sky

---
+++ lib/prl2/src/sky/hosek_sky.cpp
@@ -0,0 +1,54 @@
+#include "sky/hosek_sky.h"
+
+namespace Prl2 {
+
+HosekSky::HosekSky(const Vec3& _sunDirection, const Real& turbidity,
+                   const SPD& albedo)
+    : sunDirection(normalize(_sunDirection)) {
+  Real solarElevation, _tmp;
+  cartesianToSpherical(sunDirection, solarElevation, _tmp);
+  solarElevation = PI_DIV_2 - solarElevation;
+
+  for (int i = 0; i < SPD::LAMBDA_SAMPLES; ++i) {
+    state[i] =
+        arhosekskymodelstate_alloc_init(solarElevation, turbidity, albedo[i]);
+  }
+}
+
+HosekSky::~HosekSky() {
+  for (int i = 0; i < SPD::LAMBDA_SAMPLES; ++i) {
+    arhosekskymodelstate_free(state[i]);
+  }
+}
+
+Real HosekSky::getRadiance(const Ray& ray) const {
+  // Compute theta, gamma
+  Real theta, phi;
+  cartesianToSpherical(ray.direction, theta, phi);
+  if (theta > PI_DIV_2) return 0;
+
+  const Real gamma = radianBetween(ray.direction, sunDirection);
+
+  Real ret = 0;
+  if (ray.lambda >= 320 && ray.lambda < 720) {
+    // Compute State Index
+    const unsigned int index = (ray.lambda - SPD::LAMBDA_MIN) /
+                               (SPD::LAMBDA_MAX - SPD::LAMBDA_MIN) *
+                               SPD::LAMBDA_SAMPLES;
+
+    // gammaが太陽の視野角/2以下なら太陽光を計算
+    if (gamma < 0.251 / 180.0 * PI) {
+      ret = arhosekskymodel_solar_radiance(state[index], theta, gamma,
+                                           ray.lambda);
+    } else {
+      ret = arhosekskymodel_radiance(state[index], theta, gamma, ray.lambda);
+    }
+  }
+
+  if (std::isnan(ret)) {
+    ret = 0;
+  }
+  return ret;
+}
+
+}
--- lib/prl2/src/sky/hosek_sky.h
+++ lib/prl2/src/sky/hosek_sky.h
@@ -1,63 +1,21 @@
 #ifndef _PRL2_HOSEK_SKY
 #define _PRL2_HOSEK_SKY
 
+#include "core/spectrum.h"
+#include "sky/sky.h"
+
 extern "C" {
 #include "ArHosekSkyModel.h"
 }
-
-#include "core/spectrum.h"
-#include "sky/sky.h"
 
 namespace Prl2 {
 
 class HosekSky : public Sky {
  public:
-  HosekSky(const Vec3& _sunDirection, const Real& turbidity, const SPD& albedo)
-      : sunDirection(normalize(_sunDirection)) {
-    Real solarElevation, _tmp;
-    cartesianToSpherical(sunDirection, solarElevation, _tmp);
-    solarElevation = PI_DIV_2 - solarElevation;
+  HosekSky(const Vec3& _sunDirection, const Real& turbidity, const SPD& albedo);
+  ~HosekSky();
 
-    for (int i = 0; i < SPD::LAMBDA_SAMPLES; ++i) {
-      state[i] =
-          arhosekskymodelstate_alloc_init(solarElevation, turbidity, albedo[i]);
-    }
-  };
-  ~HosekSky() {
-    for (int i = 0; i < SPD::LAMBDA_SAMPLES; ++i) {
-      arhosekskymodelstate_free(state[i]);
-    }
-  };
-
-  Real getRadiance(const Ray& ray) const override {
-    // Compute theta, gamma
-    Real theta, phi;
-    cartesianToSpherical(ray.direction, theta, phi);
-    if (theta > PI_DIV_2) return 0;
-
-    const Real gamma = radianBetween(ray.direction, sunDirection);
-
-    Real ret = 0;
-    if (ray.lambda >= 320 && ray.lambda < 720) {
-      // Compute State Index
-      const unsigned int index = (ray.lambda - SPD::LAMBDA_MIN) /
-                                 (SPD::LAMBDA_MAX - SPD::LAMBDA_MIN) *
-                                 SPD::LAMBDA_SAMPLES;
-
-      // gammaが太陽の視野角/2以下なら太陽光を計算
-      if (gamma < 0.251 / 180.0 * PI) {
-        ret = arhosekskymodel_solar_radiance(state[index], theta, gamma,
-                                             ray.lambda);
-      } else {
-        ret = arhosekskymodel_radiance(state[index], theta, gamma, ray.lambda);
-      }
-    }
-
-    if (std::isnan(ret)) {
-      ret = 0;
-    }
-    return ret;
-  };
+  Real getRadiance(const Ray& ray) const override;
 
  private:
   ArHosekSkyModelState* state[SPD::LAMBDA_SAMPLES];
