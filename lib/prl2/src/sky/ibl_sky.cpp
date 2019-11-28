6fb5d7a0035924d9cb0f6463ad16a6e91d186d5e
yumcyawiz <hashdolf1923@gmail.com>
2019-11-28 21:10:28 +0900

    impl ibl sky

--- lib/prl2/src/sky/ibl_sky.cpp
+++ lib/prl2/src/sky/ibl_sky.cpp
@@ -17,13 +17,27 @@ IBLSky::IBLSky(const std::string& filename) {
       const float r = image[index + 0];
       const float g = image[index + 1];
       const float b = image[index + 2];
-      pixels[index] = RGB2Spectrum(RGB(r, g, b));
+      pixels[i + width * j] = RGB2Spectrum(RGB(r, g, b));
     }
   }
 }
 
 IBLSky::~IBLSky() { delete[] pixels; }
 
-Real IBLSky::getRadiance(const Ray& ray) const { return 0; }
+Real IBLSky::getRadiance(const Ray& ray) const {
+  // 球面座標を計算
+  Real theta, phi;
+  cartesianToSpherical(ray.direction, theta, phi);
+
+  // (u, v)を計算
+  const Real u = phi * INV_PI_MUL_2;
+  const Real v = theta * INV_PI;
+
+  // (i, j)を計算
+  const int i = u * width;
+  const int j = v * height;
+
+  return pixels[i + width * j].sample(ray.lambda);
+}
 
 }  // namespace Prl2
