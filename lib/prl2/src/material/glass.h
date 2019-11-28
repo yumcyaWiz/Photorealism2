a711283f81dc5f7707ca0790b6d33952d3c9bd2d
yumcyawiz <hashdolf1923@gmail.com>
2019-11-28 18:02:59 +0900

    impl glass

--- lib/prl2/src/material/glass.h
+++ lib/prl2/src/material/glass.h
@@ -37,18 +37,37 @@ class Glass : public Material {
 
   Real sampleDirection(const SurfaceInteraction& interaction, Sampler& sampler,
                        Vec3& wi_local, Real& pdf) const override {
+    const bool is_entering = -interaction.wo_local.y() > 0;
     const Real glass_ior = sellmeier.ior(interaction.lambda);
-    const Real fr = fresnel(interaction.wo_local, Vec3(0, 1, 0),
-                            interaction.ior, glass_ior);
 
-    // Reflect
+    const Vec3 normal = is_entering ? Vec3(0, 1, 0) : Vec3(0, -1, 0);
+    const Real ior1 = is_entering ? 1.0 : glass_ior;
+    const Real ior2 = is_entering ? glass_ior : 1.0;
+
+    // Fresnel Cofficient
+    const Real fr = fresnel(interaction.wo_local, normal, ior1, ior2);
+
+    // Reflection
     if (sampler.getNext() < fr) {
-      wi_local = reflect(interaction.wo_local, Vec3(0, 1, 0));
+      wi_local = reflect(interaction.wo_local, normal);
       pdf = fr;
-      return albedo.sample(interaction.lambda) / absCosTheta(wi_local);
+      return fr * albedo.sample(interaction.lambda) / absCosTheta(wi_local);
     }
     // Refract
     else {
+      Vec3 wt;
+      if (refract(interaction.wo_local, wt, normal, ior1, ior2)) {
+        wi_local = wt;
+        pdf = 1 - fr;
+        return (1 - fr) * albedo.sample(interaction.lambda) /
+               absCosTheta(wi_local);
+      }
+      // Total Refrection
+      else {
+        wi_local = reflect(interaction.wo_local, normal);
+        pdf = 1 - fr;
+        return (1 - fr) * albedo.sample(interaction.lambda) / absCosTheta(wi_local);
+      }
     }
   };
 
