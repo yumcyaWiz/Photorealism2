dfddfd3a365cfcc2e3f41dade8686ee458438f24
yumcyawiz <hashdolf1923@gmail.com>
2019-11-29 02:09:01 +0900

    remove normalization in ibl sky

--- lib/prl2/src/sky/ibl_sky.cpp
+++ lib/prl2/src/sky/ibl_sky.cpp
@@ -10,28 +10,6 @@ IBLSky::IBLSky(const std::string& filename) {
   // HDR画像の読み込み
   int c;
   pixels = stbi_loadf(filename.c_str(), &width, &height, &c, 3);
-
-  // 正規化
-  Real max = 0;
-  for (int j = 0; j < height; ++j) {
-    for (int i = 0; i < width; ++i) {
-      const Real r = pixels[3 * i + 3 * width * j];
-      const Real g = pixels[3 * i + 3 * width * j + 1];
-      const Real b = pixels[3 * i + 3 * width * j + 2];
-      const Real intensity = std::sqrt(r * r + g * g + b * b);
-      if (intensity > max) {
-        max = intensity;
-      }
-    }
-  }
-
-  for (int j = 0; j < height; ++j) {
-    for (int i = 0; i < width; ++i) {
-      pixels[3 * i + 3 * width * j] /= max;
-      pixels[3 * i + 3 * width * j + 1] /= max;
-      pixels[3 * i + 3 * width * j + 2] /= max;
-    }
-  }
 }
 
 IBLSky::~IBLSky() { stbi_image_free(pixels); }
