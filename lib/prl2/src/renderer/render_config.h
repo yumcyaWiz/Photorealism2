2d5b6a30ad10356d74296cf16436b9f6e89e143d
yumcyawiz <hashdolf1923@gmail.com>
2019-11-30 01:54:23 +0900

    add camera type to config

--- lib/prl2/src/renderer/render_config.h
+++ lib/prl2/src/renderer/render_config.h
@@ -21,6 +21,9 @@ enum class ToneMappingType { Linear, Reinhard };
 // 空の種類
 enum class SkyType { Uniform, Hosek, IBL };
 
+// Cameraの種類
+enum class CameraType { Pinhole, Environment };
+
 // レンダリングの設定を表すクラス
 // 画像のサイズ、サンプル数、カメラの種類、シーンファイルの種類などを設定する
 struct RenderConfig {
@@ -47,10 +50,12 @@ struct RenderConfig {
   Real height_length = 0.0251f;  //フィルムの縦幅[m]
 
   // Camera
-  std::string camera_type;               //カメラの種類
+  CameraType camera_type;                // カメラの種類
   Vec3 camera_position = Vec3(0, 1, 3);  // カメラの位置
   Vec3 camera_lookat = Vec3(0, 0, 0);    // カメラの注目位置
-  Real fov = 90.0f;                      // 対角FOV
+
+  // Pinhole Camera
+  Real camera_pinhole_fov = 90.0f;  // 対角FOV
 
   // Scene
   std::string scene_file;  //シーンファイル
