025cd6a2be34305fd54efee63e6ba25636ce4abd
yumcyawiz <hashdolf1923@gmail.com>
2019-11-29 20:44:18 +0900

    add environment camera

---
+++ lib/prl2/src/camera/environment.h
@@ -0,0 +1,17 @@
+#ifndef _PRL2_ENVIRONMENT_H
+#define _PRL2_ENVIRONMENT_H
+
+#include "camera/camera.h"
+
+namespace Prl2 {
+class EnvironmentCamera : public Camera {
+ public:
+  EnvironmentCamera(const std::shared_ptr<Film>& _film,
+                    const std::shared_ptr<Transform>& _localToWorld);
+
+  bool generateRay(const Real& u, const Real& v, Ray& ray) const = 0;
+};
+
+}  // namespace Prl2
+
+#endif
