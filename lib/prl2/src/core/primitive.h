753a9850f1cc9395ab5dcd1885ead033cd5b54d1
yumcyawiz <hashdolf1923@gmail.com>
2019-12-03 02:46:01 +0900

    add light to primitive

--- lib/prl2/src/core/primitive.h
+++ lib/prl2/src/core/primitive.h
@@ -7,6 +7,7 @@
 #include "core/ray.h"
 
 #include "core/geometry.h"
+#include "light/light.h"
 #include "material/material.h"
 
 namespace Prl2 {
@@ -14,10 +15,11 @@ namespace Prl2 {
 class Primitive {
  public:
   Primitive(const std::shared_ptr<Geometry>& _geometry,
-            const std::shared_ptr<Material>& _material)
-      : geometry(_geometry), material(_material){};
+            const std::shared_ptr<Material>& _material,
+            const std::shared_ptr<Light>& _light = nullptr)
+      : geometry(_geometry), material(_material), light(_light) {};
 
-  //ワールド座標系のレイを受け取り、Geometryとの衝突計算を行う。結果をinfoに保存する。
+  // ワールド座標系のレイを受け取り、Geometryとの衝突計算を行う。結果をinfoに保存する。
   bool intersect(const Ray& ray, IntersectInfo& info) const {
     if (geometry->intersect(ray, info)) {
       // 衝突Primitiveをセット
@@ -30,6 +32,7 @@ class Primitive {
 
   const std::shared_ptr<Geometry> geometry;  // Geometry
   const std::shared_ptr<Material> material;  // Material
+  const std::shared_ptr<Light> light;        // Light
 };
 
 }  // namespace Prl2
