a7acac744d59cd51dd32b2ecefe2b7dd6816958c
yumcyawiz <hashdolf1923@gmail.com>
2019-12-03 02:24:54 +0900

    use geometry in primitive

--- lib/prl2/src/core/primitive.h
+++ lib/prl2/src/core/primitive.h
@@ -5,45 +5,31 @@
 
 #include "core/isect.h"
 #include "core/ray.h"
-#include "core/transform.h"
 
 #include "core/geometry.h"
 #include "material/material.h"
-#include "shape/shape.h"
 
 namespace Prl2 {
 
 class Primitive {
  public:
-  Primitive(const std::shared_ptr<Shape>& _shape,
-            const std::shared_ptr<Material>& _material,
-            const std::shared_ptr<Transform>& _localToWorld)
-      : shape(_shape), material(_material), localToWorld(_localToWorld){};
+  Primitive(const std::shared_ptr<Geometry>& _geometry,
+            const std::shared_ptr<Material>& _material)
+      : geometry(_geometry), material(_material){};
 
-  //ワールド座標系のレイを受け取り、shapeとの衝突計算を行う。結果をinfoに保存する。
+  //ワールド座標系のレイを受け取り、Geometryとの衝突計算を行う。結果をinfoに保存する。
   bool intersect(const Ray& ray, IntersectInfo& info) const {
-    //レイをローカル座標系に変換
-    const Ray ray_local = localToWorld->applyInverse(ray);
-
-    // shapeとの衝突計算
-    IntersectInfo info_local;
-    if (shape->intersect(ray_local, info_local)) {
-      // ローカル座標系の衝突情報をワールド座標系に変換
-      info = localToWorld->apply(info_local);
-
+    if (geometry->intersect(ray, info)) {
       // 衝突Primitiveをセット
       info.hitPrimitive = this;
-
       return true;
     } else {
       return false;
     }
   };
 
-  const std::shared_ptr<Shape> shape;        // Shape
+  const std::shared_ptr<Geometry> geometry;  // Geometry
   const std::shared_ptr<Material> material;  // Material
-  const std::shared_ptr<Transform>
-      localToWorld;  //ローカル座標系からワールド座標系への変換を表すTransform
 };
 
 }  // namespace Prl2
