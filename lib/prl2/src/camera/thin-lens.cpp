8525650c8a643f2b70cf5673b506d86cd9086f50
yumcyawiz <hashdolf1923@gmail.com>
2019-12-08 01:07:57 +0900

    impl thin lens constructor

--- lib/prl2/src/camera/thin-lens.cpp
+++ lib/prl2/src/camera/thin-lens.cpp
@@ -4,11 +4,17 @@
 namespace Prl2 {
 
 ThinLensCamera::ThinLensCamera(const std::shared_ptr<Film>& _film,
-                               const std::shared_ptr<Transform>& _localToWorld)
-    : Camera(_film, _localToWorld), fov(degToRad(90.0f)) {}
+                               const std::shared_ptr<Transform>& _localToWorld,
+                               const Real& _fov, const Real& _thin_lens_radius)
+    : Camera(_film, _localToWorld),
+      fov(degToRad(_fov)),
+      thin_lens_radius(_thin_lens_radius) {
+  thin_lens_pos = 0.5f * film->width_length / std::tan(0.5f * fov);
+}
 
-bool ThinLensCamera::generateRay(const Vec2& pFilm, Sampler& sampler, Ray& ray,
-                                 Real& pdf) const {
+bool ThinLensCamera::generateRay(const Vec2& pFilm_2d, Sampler& sampler,
+                                 Ray& ray, Real& pdf) const {
+  const Vec3 pFilm(pFilm_2d.x(), pFilm_2d.y(), 0);
   return false;
 }
 
--- lib/prl2/src/camera/thin-lens.h
+++ lib/prl2/src/camera/thin-lens.h
@@ -8,13 +8,16 @@ namespace Prl2 {
 class ThinLensCamera : public Camera {
  public:
   ThinLensCamera(const std::shared_ptr<Film>& _film,
-                 const std::shared_ptr<Transform>& _localToWorld);
+                 const std::shared_ptr<Transform>& _localToWorld,
+                 const Real& _fov, const Real& _thin_lens_radius);
 
   bool generateRay(const Vec2& pFilm, Sampler& sampler, Ray& ray,
                    Real& pdf) const override;
 
  private:
-  const Real fov;  // 横視野角
+  const Real fov;               // 横視野角
+  const Real thin_lens_radius;  // Thin-Lensの半径
+  Real thin_lens_pos;           // Thin-Lensの位置
 };
 
 }  // namespace Prl2
