f07c110073ca5630ed0531a6b41287f7f842720e
yumcyawiz <hashdolf1923@gmail.com>
2019-12-07 20:28:41 +0900

    add thin lens cpp

--- lib/prl2/src/camera/thin-lens.cpp
+++ lib/prl2/src/camera/thin-lens.cpp
@@ -1,1 +1,14 @@
 #include "camera/thin-lens.h"
+#include "core/util.h"
+
+namespace Prl2 {
+
+ThinLensCamera::ThinLensCamera(const std::shared_ptr<Film>& _film,
+                               const std::shared_ptr<Transform>& _localToWorld)
+    : Camera(_film, _localToWorld), fov(degToRad(90.0f)) {}
+
+bool ThinLensCamera::generateRay(const Real& u, const Real& v, Ray& ray) const {
+  return false;
+}
+
+}  // namespace Prl2
