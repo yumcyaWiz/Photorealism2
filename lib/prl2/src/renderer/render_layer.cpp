6afdd3dd5e7395de00b31dcd25a0dc856bde2c77
yumcyawiz <hashdolf1923@gmail.com>
2019-12-09 08:31:52 +0900

    separate compile render layer

---
+++ lib/prl2/src/renderer/render_layer.cpp
@@ -0,0 +1,70 @@
+#include "renderer/render_layer.h"
+
+namespace Prl2 {
+
+RenderLayer::RenderLayer(const RenderConfig& config) {
+  render_sRGB.resize(3 * config.width * config.height);
+  denoised_sRGB.resize(3 * config.width * config.height);
+  albedo_sRGB.resize(3 * config.width * config.height);
+  normal_sRGB.resize(3 * config.width * config.height);
+  depth_sRGB.resize(3 * config.width * config.height);
+  position_sRGB.resize(3 * config.width * config.height);
+  samples.resize(config.width * config.height);
+  sample_sRGB.resize(3 * config.width * config.height);
+}
+
+void RenderLayer::resize(int width, int height) {
+  render_sRGB.resize(3 * width * height);
+  denoised_sRGB.resize(3 * width * height);
+  albedo_sRGB.resize(3 * width * height);
+  normal_sRGB.resize(3 * width * height);
+  depth_sRGB.resize(3 * width * height);
+  position_sRGB.resize(3 * width * height);
+  samples.resize(width * height);
+  sample_sRGB.resize(3 * width * height);
+}
+
+void RenderLayer::clear() {
+  std::fill(render_sRGB.begin(), render_sRGB.end(), 0);
+  std::fill(denoised_sRGB.begin(), denoised_sRGB.end(), 0);
+  std::fill(albedo_sRGB.begin(), albedo_sRGB.end(), 0);
+  std::fill(normal_sRGB.begin(), normal_sRGB.end(), 0);
+  std::fill(depth_sRGB.begin(), depth_sRGB.end(), 0);
+  std::fill(position_sRGB.begin(), position_sRGB.end(), 0);
+  std::fill(samples.begin(), samples.end(), 1);
+  std::fill(sample_sRGB.begin(), sample_sRGB.end(), 0);
+}
+
+void RenderLayer::clearPixel(int i, int j, int width, int height) {
+  render_sRGB[3 * i + 3 * width * j] = 0;
+  render_sRGB[3 * i + 3 * width * j + 1] = 0;
+  render_sRGB[3 * i + 3 * width * j + 2] = 0;
+
+  denoised_sRGB[3 * i + 3 * width * j] = 0;
+  denoised_sRGB[3 * i + 3 * width * j + 1] = 0;
+  denoised_sRGB[3 * i + 3 * width * j + 2] = 0;
+
+  albedo_sRGB[3 * i + 3 * width * j] = 0;
+  albedo_sRGB[3 * i + 3 * width * j + 1] = 0;
+  albedo_sRGB[3 * i + 3 * width * j + 2] = 0;
+
+  normal_sRGB[3 * i + 3 * width * j] = 0;
+  normal_sRGB[3 * i + 3 * width * j + 1] = 0;
+  normal_sRGB[3 * i + 3 * width * j + 2] = 0;
+
+  depth_sRGB[3 * i + 3 * width * j] = 0;
+  depth_sRGB[3 * i + 3 * width * j + 1] = 0;
+  depth_sRGB[3 * i + 3 * width * j + 2] = 0;
+
+  position_sRGB[3 * i + 3 * width * j] = 0;
+  position_sRGB[3 * i + 3 * width * j + 1] = 0;
+  position_sRGB[3 * i + 3 * width * j + 2] = 0;
+
+  samples[i + width * j] = 1;  // 0割りを防ぐためにサンプル数は1で初期化する
+
+  sample_sRGB[3 * i + 3 * width * j] = 0;
+  sample_sRGB[3 * i + 3 * width * j + 1] = 0;
+  sample_sRGB[3 * i + 3 * width * j + 2] = 0;
+}
+
+}  // namespace Prl2
--- lib/prl2/src/renderer/render_layer.h
+++ lib/prl2/src/renderer/render_layer.h
@@ -14,73 +14,16 @@ struct RenderLayer {
   RenderLayer(){};
 
   // RenderConfigから画像を初期化
-  RenderLayer(const RenderConfig& config) {
-    render_sRGB.resize(3 * config.width * config.height);
-    denoised_sRGB.resize(3 * config.width * config.height);
-    albedo_sRGB.resize(3 * config.width * config.height);
-    normal_sRGB.resize(3 * config.width * config.height);
-    depth_sRGB.resize(3 * config.width * config.height);
-    position_sRGB.resize(3 * config.width * config.height);
-    samples.resize(config.width * config.height);
-    sample_sRGB.resize(3 * config.width * config.height);
-  };
+  RenderLayer(const RenderConfig& config);
 
   // リサイズする
-  void resize(int width, int height) {
-    render_sRGB.resize(3 * width * height);
-    denoised_sRGB.resize(3 * width * height);
-    albedo_sRGB.resize(3 * width * height);
-    normal_sRGB.resize(3 * width * height);
-    depth_sRGB.resize(3 * width * height);
-    position_sRGB.resize(3 * width * height);
-    samples.resize(width * height);
-    sample_sRGB.resize(3 * width * height);
-  };
+  void resize(int width, int height);
 
   // クリアする
-  void clear() {
-    std::fill(render_sRGB.begin(), render_sRGB.end(), 0);
-    std::fill(denoised_sRGB.begin(), denoised_sRGB.end(), 0);
-    std::fill(albedo_sRGB.begin(), albedo_sRGB.end(), 0);
-    std::fill(normal_sRGB.begin(), normal_sRGB.end(), 0);
-    std::fill(depth_sRGB.begin(), depth_sRGB.end(), 0);
-    std::fill(position_sRGB.begin(), position_sRGB.end(), 0);
-    std::fill(samples.begin(), samples.end(), 1);
-    std::fill(sample_sRGB.begin(), sample_sRGB.end(), 0);
-  };
+  void clear();
 
   // 指定したピクセルのデータだけクリアする
-  void clearPixel(int i, int j, int width, int height) {
-    render_sRGB[3 * i + 3 * width * j] = 0;
-    render_sRGB[3 * i + 3 * width * j + 1] = 0;
-    render_sRGB[3 * i + 3 * width * j + 2] = 0;
-
-    denoised_sRGB[3 * i + 3 * width * j] = 0;
-    denoised_sRGB[3 * i + 3 * width * j + 1] = 0;
-    denoised_sRGB[3 * i + 3 * width * j + 2] = 0;
-
-    albedo_sRGB[3 * i + 3 * width * j] = 0;
-    albedo_sRGB[3 * i + 3 * width * j + 1] = 0;
-    albedo_sRGB[3 * i + 3 * width * j + 2] = 0;
-
-    normal_sRGB[3 * i + 3 * width * j] = 0;
-    normal_sRGB[3 * i + 3 * width * j + 1] = 0;
-    normal_sRGB[3 * i + 3 * width * j + 2] = 0;
-
-    depth_sRGB[3 * i + 3 * width * j] = 0;
-    depth_sRGB[3 * i + 3 * width * j + 1] = 0;
-    depth_sRGB[3 * i + 3 * width * j + 2] = 0;
-
-    position_sRGB[3 * i + 3 * width * j] = 0;
-    position_sRGB[3 * i + 3 * width * j + 1] = 0;
-    position_sRGB[3 * i + 3 * width * j + 2] = 0;
-
-    samples[i + width * j] = 1;  // 0割りを防ぐためにサンプル数は1で初期化する
-
-    sample_sRGB[3 * i + 3 * width * j] = 0;
-    sample_sRGB[3 * i + 3 * width * j + 1] = 0;
-    sample_sRGB[3 * i + 3 * width * j + 2] = 0;
-  };
+  void clearPixel(int i, int j, int width, int height);
 
   std::vector<Real> render_sRGB;  // レンダリング結果をsRGBにしたものを格納する
   std::vector<Real> denoised_sRGB;  // デノイズされたsRGBを格納する
