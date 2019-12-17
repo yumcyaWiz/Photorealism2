3efa15db3fb004fab074cfa273fff7f0ed142ab3
yumcyawiz <hashdolf1923@gmail.com>
2019-12-18 04:11:40 +0900

    fix fragment shader

--- app/src/gui.cpp
+++ app/src/gui.cpp
@@ -24,9 +24,10 @@ const std::string GUI::showpath_vertex_shader_source = R"(
 
 const std::string GUI::showpath_fragment_shader_source = R"(
   #version 330 core
+  layout(location = 0) out vec3 color;
 
   void main() {
-    gl_FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+    color = vec3(1.0f, 1.0f, 0.0f);
   }
 )";
 
