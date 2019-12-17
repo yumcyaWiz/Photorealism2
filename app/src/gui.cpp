0d7a7de61e9a20ed04a93821390f4f9ad2eec17a
yumcyawiz <hashdolf1923@gmail.com>
2019-12-18 04:04:48 +0900

    showpath draw call

--- app/src/gui.cpp
+++ app/src/gui.cpp
@@ -603,9 +603,17 @@ void GUI::showPath(int i, int j, const Render& render) const {
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT),
                         (GLvoid*)0);
+
   glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
+  // Draw
+  glUseProgram(showpath_program);
   glBindFramebuffer(GL_FRAMEBUFFER, framebuffer_id);
-
+  glDrawArrays(GL_LINE, 0, vertices.size());
   glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+  // Delete Buffers
+  glDeleteBuffers(1, &vertex_vbo);
+  glDeleteBuffers(1, &vao);
 }
