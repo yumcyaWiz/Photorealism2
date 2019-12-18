386b9622e8535ccf89eb1eafd6bc71bfec5e4e15
yumcyawiz <hashdolf1923@gmail.com>
2019-12-18 16:42:09 +0900

    add image frame buffer

--- app/src/gui.cpp
+++ app/src/gui.cpp
@@ -90,6 +90,20 @@ GUI::GUI() {
   // Image
   // Image Textureの用意
   glGenTextures(1, &image_texture);
+  glBindTexture(GL_TEXTURE_2D, image_texture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 512, 512, 0, GL_RGB, GL_UNSIGNED_BYTE,
+               0);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  // Image Frame Buffer
+  glGenFramebuffers(1, &image_framebuffer);
+  glBindFramebuffer(GL_FRAMEBUFFER, image_framebuffer);
+  glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, image_texture, 0);
+  GLenum image_drawbuffers[1] = {GL_COLOR_ATTACHMENT0};
+  glDrawBuffers(1, image_drawbuffers);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
   // Fragment Shader
   GLuint image_fragment_shader =
