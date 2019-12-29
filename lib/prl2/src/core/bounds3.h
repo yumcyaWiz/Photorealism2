129f2189d5ad84701144cdbed12678a772dccc21
yumcyawiz <hashdolf1923@gmail.com>
2019-12-30 04:51:28 +0900

    fix bounds3 constructor

--- lib/prl2/src/core/bounds3.h
+++ lib/prl2/src/core/bounds3.h
@@ -7,11 +7,19 @@ namespace Prl2 {
 
 class Bounds3 {
  public:
-  const Vec3 p1;
-  const Vec3 p2;
+  Vec3 p1;
+  Vec3 p2;
 
   Bounds3(){};
-  Bounds3(const Vec3& _p1, const Vec3& _p2) : p1(_p1), p2(_p2){};
+  Bounds3(const Vec3& _p1, const Vec3& _p2) {
+    p1[0] = std::min(_p1.x(), _p2.x());
+    p1[1] = std::min(_p1.y(), _p2.y());
+    p1[2] = std::min(_p1.z(), _p2.z());
+
+    p2[0] = std::max(_p1.x(), _p2.x());
+    p2[1] = std::max(_p1.y(), _p2.y());
+    p2[2] = std::max(_p1.z(), _p2.z());
+  };
 };
 
 }  // namespace Prl2
