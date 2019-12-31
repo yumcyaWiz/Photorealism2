d5df665712f240e7acfd0c57e2e3e6aab0ef05d7
yumcyawiz <hashdolf1923@gmail.com>
2019-12-31 09:59:39 +0900

    add triangle mesh

--- lib/prl2/src/shape/triangle.h
+++ lib/prl2/src/shape/triangle.h
@@ -1,13 +1,26 @@
 #ifndef _PRL2_TRIANGLE_H
 #define _PRL2_TRIANGLE_H
 
+#include "core/type.h"
+
 namespace Prl2 {
+
+// 三角形メッシュを表す
+struct TriangleMesh {
+  unsigned int num_vertices;  // 頂点数
+  unsigned int num_faces;     // 面の数
+  Real* vertices;             // 頂点配列
+  Real* indices;              // 頂点インデックス配列
+  Real* normals;              // 頂点法線
+  Real* uvs;                  // 頂点UV
+};
 
 class Triangle {
  public:
   Triangle(unsigned int _face_index);
 
  private:
+  TriangleMesh* mesh;             // TriangleMesh
   const unsigned int face_index;  // TriangleMesh中の面インデックス
 };
 
