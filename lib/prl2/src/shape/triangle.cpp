cb8203d9faf26c5d8973ba9511966e077c8f05f4
yumcyawiz <hashdolf1923@gmail.com>
2019-12-31 09:46:27 +0900

    add triangle shape

---
+++ lib/prl2/src/shape/triangle.cpp
@@ -0,0 +1,3 @@
+#include "shape/triangle.h"
+
+namespace Prl2 {}
---
+++ lib/prl2/src/shape/triangle.h
@@ -0,0 +1,12 @@
+#ifndef _PRL2_TRIANGLE_H
+#define _PRL2_TRIANGLE_H
+
+namespace Prl2 {
+
+class Triangle {
+ public:
+  Triangle(){};
+};
+
+}  // namespace Prl2
+#endif
