9d0eb06cec5cd65e4e949f3ab5a3dea0c9aa3003
yumcyawiz <hashdolf1923@gmail.com>
2020-01-02 01:52:21 +0900

    impli triangle intersection

--- lib/prl2/src/shape/triangle.cpp
+++ lib/prl2/src/shape/triangle.cpp
@@ -6,7 +6,50 @@ Triangle::Triangle(const std::shared_ptr<TriangleMesh>& _mesh,
                    unsigned int _face_index)
     : mesh(_mesh), face_index(_face_index) {}
 
-bool Triangle::intersect(const Ray& ray, IntersectInfo& info) const {}
+// Möller–Trumbore intersection algorithm
+// https://www.wikiwand.com/en/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+bool Triangle::intersect(const Ray& ray, IntersectInfo& info) const {
+  const unsigned int f0 = mesh->indices[3 * face_index];
+  const unsigned int f1 = mesh->indices[3 * face_index + 1];
+  const unsigned int f2 = mesh->indices[3 * face_index + 2];
+
+  const Vec3& v0 = mesh->vertices[f0];
+  const Vec3& v1 = mesh->vertices[f1];
+  const Vec3& v2 = mesh->vertices[f2];
+
+  const Vec3 edge1 = v1 - v0;
+  const Vec3 edge2 = v2 - v0;
+  const Vec3 h = cross(ray.direction, edge2);
+
+  const Real a = dot(edge1, h);
+  if (a > -EPS && a < EPS) {
+    return false;
+  }
+
+  const Real f = 1.0f / a;
+  const Vec3 s = ray.origin - v0;
+  const Real u = f * dot(s, h);
+  if (u < 0.0f || u > 1.0f) {
+    return false;
+  }
+
+  const Vec3 q = cross(s, edge1);
+  const Real v = f * dot(ray.direction, q);
+
+  if (v < 0.0f || u + v > 1.0f) {
+    return false;
+  }
+
+  const Real t = f * dot(edge2, q);
+  if (t < ray.tmin || t > ray.tmax) {
+    return false;
+  }
+
+  info.t = t;
+  info.hitPos = ray(t);
+
+  return false;
+}
 
 bool Triangle::occluded(const Ray& ray) const { return false; }
 
