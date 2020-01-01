6e26f0ba991a4962e8340e847ce137979e37bb3b
yumcyawiz <hashdolf1923@gmail.com>
2020-01-02 01:21:07 +0900

    add triangle func impls

--- lib/prl2/src/shape/triangle.cpp
+++ lib/prl2/src/shape/triangle.cpp
@@ -5,4 +5,15 @@ namespace Prl2 {
 Triangle::Triangle(const TriangleMesh* _mesh, unsigned int _face_index)
     : mesh(_mesh), face_index(_face_index) {}
 
+bool Triangle::intersect(const Ray& ray, IntersectInfo& info) const {
+  return false;
+}
+
+bool Triangle::occluded(const Ray& ray) const { return false; }
+
+Bounds3 Triangle::getBounds() const { return Bounds3(); }
+
+void Triangle::samplePoint(Sampler& sampler, Vec3& p, Vec3& n,
+                           Real& pdf_area) const {}
+
 }  // namespace Prl2
