9a4334c3f14ef30eaf888c2e0554a82bfe45f995
yumcyawiz <hashdolf1923@gmail.com>
2020-01-02 20:29:17 +0900

    impli load obj

--- lib/prl2/src/renderer/scene-loader.cpp
+++ lib/prl2/src/renderer/scene-loader.cpp
@@ -1,7 +1,43 @@
 #include "renderer/scene-loader.h"
 
+#include "tiny_obj_loader.h"
+
 namespace Prl2 {
 
-bool loadTriangleMeshFromObj(const std::string& filename) { return false; }
+bool loadTriangleMeshFromObj(const std::string& filename) {
+  tinyobj::attrib_t attrib;
+  std::vector<tinyobj::shape_t> shapes;
+  std::vector<tinyobj::material_t> materials;
+
+  std::string warn;
+  std::string err;
+
+  bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err,
+                              filename.c_str());
+
+  if (!warn.empty()) {
+    std::cout << warn << std::endl;
+  }
+
+  if (!err.empty()) {
+    std::cout << err << std::endl;
+  }
+
+  if (!ret) {
+    return false;
+  }
+
+  for (size_t s = 0; s < shapes.size(); ++s) {
+    size_t index_offset = 0;
+    for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); ++f) {
+      const size_t fv = shapes[s].mesh.num_face_vertices[f];
+      for (size_t v = 0; v < fv; ++v) {
+        tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+      }
+    }
+  }
+
+  return true;
+}
 
 }  // namespace Prl2
