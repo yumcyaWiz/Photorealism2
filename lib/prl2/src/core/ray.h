95212789c0f9d749289292d81e2baa5fcfffc32b
yumcyawiz <hashdolf1923@gmail.com>
2020-02-12 09:08:15 +0900

    fix -Wextra-semi

--- lib/prl2/src/core/ray.h
+++ lib/prl2/src/core/ray.h
@@ -13,14 +13,14 @@ struct Ray {
   Vec3 direction;  // 方向
   Real lambda;     // 波長[nm]
 
-  static constexpr Real tmin = 1e-6;  //最小衝突距離
-  static constexpr Real tmax = 1e6;   //最大衝突距離
+  static constexpr Real tmin = 1e-6f;  //最小衝突距離
+  static constexpr Real tmax = 1e6f;   //最大衝突距離
 
-  Ray(){};
+  Ray() {}
   Ray(const Vec3& _origin, const Vec3& _direction, const Real& _lambda = 550)
-      : origin(_origin), direction(_direction), lambda(_lambda){};
+      : origin(_origin), direction(_direction), lambda(_lambda) {}
 
-  Vec3 operator()(const Real& t) const { return origin + t * direction; };
+  Vec3 operator()(const Real& t) const { return origin + t * direction; }
 };
 
 inline std::ostream& operator<<(std::ostream& stream, const Ray& ray) {
