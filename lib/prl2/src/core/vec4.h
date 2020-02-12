4d04f9b810ddb5bc8ff26fc7e077c8c0c8c3ecc7
yumcyawiz <hashdolf1923@gmail.com>
2020-02-12 09:17:37 +0900

    fix -Wextra-semi

--- lib/prl2/src/core/vec4.h
+++ lib/prl2/src/core/vec4.h
@@ -10,11 +10,11 @@ namespace Prl2 {
 
 class alignas(16) Vec4 {
  public:
-  explicit Vec4() { v[0] = v[1] = v[2] = v[3] = 0; };
+  explicit Vec4() { v[0] = v[1] = v[2] = v[3] = 0; }
   explicit Vec4(const Real& _x) {
     assert(!std::isnan(_x));
     v[0] = v[1] = v[2] = v[3] = _x;
-  };
+  }
   explicit Vec4(const Real& _x, const Real& _y, const Real& _z,
                 const Real& _w) {
     assert(!std::isnan(_x) && !std::isnan(_y) && !std::isnan(_z) &&
@@ -23,17 +23,17 @@ class alignas(16) Vec4 {
     v[1] = _y;
     v[2] = _z;
     v[3] = _w;
-  };
+  }
 
   Real& operator[](int i) {
     assert(i >= 0 && i < 4);
     return v[i];
-  };
+  }
 
-  Real x() const { return v[0]; };
-  Real y() const { return v[1]; };
-  Real z() const { return v[2]; };
-  Real w() const { return v[3]; };
+  Real x() const { return v[0]; }
+  Real y() const { return v[1]; }
+  Real z() const { return v[2]; }
+  Real w() const { return v[3]; }
 
  private:
   Real v[4];
